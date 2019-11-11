8cbcb88c7156f8c7eb5ceb2c5847e8880b9d53b0
dwesty17 <j.dylan.westerhold@gmail.com>
2019-11-10 22:22:08 -0800

    Storing array for output csv - need to write it next

--- transaction_log/transaction_log.py
+++ transaction_log/transaction_log.py
@@ -12,14 +12,19 @@ class TransactionLog:
         print("")
         current_date = self.transactions[0]["transaction_date"]
 
+        payment_count_today = 0
         amount_spent_today = Decimal(0)
+        csv_out = []
         for transaction in self.transactions:
             if transaction["transaction_date"].day != current_date.day:
                 print(current_date.strftime("%m-%d-%Y") + ": $" + str(amount_spent_today))
-                amount_spent_today = Decimal(0)
+                csv_out.append([current_date.strftime("%m-%d-%Y"), amount_spent_today, payment_count_today])
                 current_date = transaction["transaction_date"]
-            else:
-                amount_spent_today -= transaction["total_amount"] if transaction["type"] == "CREDIT" else Decimal(0)
+                amount_spent_today = Decimal(0)
+                payment_count_today = 0
+            elif transaction["type"] == "CREDIT":
+                amount_spent_today -= transaction["total_amount"]
+                payment_count_today += 1
 
     def add_statement(self, statement):
         self.starting_balance += statement["starting_balance"]
